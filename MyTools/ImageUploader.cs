f94c22562be8a59b9b97896ee7c1b557cb690b93
bkarapinar <57687748+bkarapinar@users.noreply.github.com>
2019-11-13 01:28:20 +0300

    Add files via upload

---
+++ MyTools/ImageUploader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Project.COMMON.MyTools
+{
+    public static class ImageUploader
+    {
+        public static string UploadImage(string serverPath, HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                Guid uniqueName = Guid.NewGuid();
+
+                serverPath = serverPath.Replace("~", string.Empty); //Gelen serverPath parametresinde tilda işareti varsa boşlukla değştiriyoruz.
+
+                string[] fileArray = file.FileName.Split('.'); // Gelen veriyi '.' olan yerlerden ayırarak elemanları string dizisine dönüştürdük.
+
+                string extension = fileArray[fileArray.Length - 1].ToLower(); // Dosya uzantısını yakaladık ve küçük harflere çevirdik.
+
+                string fileName = $"{uniqueName}.{extension}"; // Oluşturduğumuz Guid tipindeki değişkenimizle yakaladığımız extension u "dosyaadi.uzantisi" şekline dönüştürdük.
+
+                if (extension == "jpg" || extension == "gif" || extension == "png" || extension == "jpeg") //Uzantısını kontrol ederek resim formatında olup olmadığına bakıyoruz.
+                {
+                    if (File.Exists(HttpContext.Current.Server.MapPath(serverPath + fileName))) //File.Exists ile dosyanın olup olmadığını kontrol ettik
+                    {
+                        return "Dosya Daha Önce Kaydedilmiş";
+                    }
+                    else
+                    {
+                        string filePath = HttpContext.Current.Server.MapPath(serverPath + fileName);
+                        file.SaveAs(filePath);
+                        return serverPath + fileName;
+                    }
+                }
+                else
+                {
+                    return "Lütfen resim formatında bir dosya seçin!";
+                }
+
+            }
+            else
+            {
+                return "Dosya Boş";
+            }
+        }
+    }
+}
---
+++ MyTools/MailSender.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.COMMON.MyTools
+{
+    public static class MailSender
+    {
+        public static void Send(string receiver, string password, string body = "Deneme", string subject = "Test", string sender = "mymvcproject@outlook.com") 
+        {
+            MailAddress senderEmail = new MailAddress(sender);
+            MailAddress receiverEmail = new MailAddress(receiver); //item.Email
+
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = "smtp-mail.outlook.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(senderEmail.Address, password)
+            };
+
+            using (var mesaj = new MailMessage(senderEmail, receiverEmail)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(mesaj); //mail gönderildi
+            }
+        }
+    }
+}
